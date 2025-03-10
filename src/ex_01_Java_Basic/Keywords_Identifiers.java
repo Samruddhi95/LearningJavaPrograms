4e3cd63064cfd555dc3efb31a0be429575be34f8
samruddhi <samruddhi.ubhad@jiem.in>
2025-03-10 17:36:32 +0530

    Added new file for Keywords and Identifiers

---
+++ src/ex_01_Java_Basic/Keywords_Identifiers.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+//Author - Samruddhi
+//#### Differences between Keywords & Identifiers
+//Keywords-Simple English words having predefined meaning in Java Programming Language
+//Ex-- int, class, public, static, void, if, else, while,for
+
+public class Keywords_Identifiers {
+    public static void main(String[] args) {
+        /*
+        Identifiers- names those will be used to identify the programming elements
+        like classes, methods, variables, etc uniquely
+        Identifier can contain Alphabets, Digits, and two special symbol
+        First character of an identifier must be an Alphabet or $ or _
+         */
+        System.out.println("Keywords & Identifiers");
+    }
+
+}
